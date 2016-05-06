eb03a2da02d4acf15bac7bf43e9182d74b9b773c
Eric Fode <ericfode@gmail.com>
2016-05-06 10:29:53 -0700

    Update js-yaml.ext.js

--- js-yaml/resources/cljsjs/js-yaml/common/js-yaml.ext.js
+++ js-yaml/resources/cljsjs/js-yaml/common/js-yaml.ext.js
@@ -1,7 +1,30 @@
-var jsyaml = {};
-jsyaml.load        =  function () {};
-jsyaml.loadAll     =  function () {};
-jsyaml.safeLoad    =  function () {};
-jsyaml.safeLoadAll =  function () {};
-jsyaml.dump        =  function () {};
-jsyaml.safeDump    =  function () {};
+var jsyaml = {
+  "Type": function () {},
+  "Schema": {
+    "DEFAULT": {},
+    "create": function () {}
+  },
+  "FAILSAFE_SCHEMA": {},
+  "JSON_SCHEMA": {},
+  "CORE_SCHEMA": {},
+  "DEFAULT_SAFE_SCHEMA": {},
+  "DEFAULT_FULL_SCHEMA": {},
+  "load": function () {},
+  "loadAll": function () {},
+  "safeLoad": function () {},
+  "safeLoadAll": function () {},
+  "dump": function () {},
+  "safeDump": function () {},
+  "YAMLException": function () {},
+  "MINIMAL_SCHEMA": {},
+  "SAFE_SCHEMA": {},
+  "DEFAULT_SCHEMA": {},
+  "scan": function () {},
+  "parse": function () {},
+  "compose": function () {},
+  "addConstructor": function () {}
+};
+jsyaml.YAMLException.prototype = {
+  "constructor": function () {},
+  "toString": function () {}
+};
